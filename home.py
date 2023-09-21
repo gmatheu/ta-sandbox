33279a5437bb42248c905805b91d173f37468dac
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2023-09-21 12:31:57 -0300

    Boostrapping streamlit app

---
+++ home.py
@@ -0,0 +1,293 @@
+import streamlit as st
+from datetime import timedelta
+from typing import Dict, Tuple
+
+import streamlit as st
+st.set_page_config(
+   page_title="Ex-stream-ly Cool App",
+   page_icon="🧊",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
+
+def intro():
+    import streamlit as st
+
+    st.write("# Welcome to Streamlit! 👋")
+    st.sidebar.success("Select a demo above.")
+
+    st.markdown(
+        """
+        Streamlit is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+
+        **👈 Select a demo from the dropdown on the left** to see some examples
+        of what Streamlit can do!
+
+        ### Want to learn more?
+
+        - Check out [streamlit.io](https://streamlit.io)
+        - Jump into our [documentation](https://docs.streamlit.io)
+        - Ask a question in our [community
+          forums](https://discuss.streamlit.io)
+
+        ### See more complex demos
+
+        - Use a neural net to [analyze the Udacity Self-driving Car Image
+          Dataset](https://github.com/streamlit/demo-self-driving)
+        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    """
+    )
+
+def plotting_demo():
+    import streamlit as st
+    import time
+    import numpy as np
+
+    st.markdown(f'# {list(page_names_to_funcs.keys())[1]}')
+    st.write(
+        """
+        This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!
+"""
+    )
+
+    progress_bar = st.sidebar.progress(0)
+    status_text = st.sidebar.empty()
+    last_rows = np.random.randn(1, 1)
+    chart = st.line_chart(last_rows)
+
+    for i in range(1, 101):
+        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+        status_text.text("%i%% Complete" % i)
+        chart.add_rows(new_rows)
+        progress_bar.progress(i)
+        last_rows = new_rows
+        time.sleep(0.05)
+
+    progress_bar.empty()
+
+    # Streamlit widgets automatically run the script from top to bottom. Since
+    # this button is not connected to any other logic, it just causes a plain
+    # rerun.
+    st.button("Re-run")
+
+def summary():
+    from notebooks.alerts import all_trades, TradeSet
+    import streamlit as st
+    import pandas as pd
+    import altair as alt
+
+    from urllib.error import URLError
+
+    st.markdown(f"# {list(page_names_to_funcs.keys())[0]}")
+    st.write(""" """)
+
+    timeframe = "D"
+    tickers = [
+        "NVDAD.BA",
+        "SATLD.BA",
+        "TSLAD.BA",
+        "GOLDD.BA",
+        "GOGLD.BA",
+        "NFLXD.BA",
+        "FCXD.BA",
+        "BABAD.BA",
+        "METAD.BA",
+        "MELID.BA",
+        "INTCD.BA",
+        "AMDD.BA",
+        "TSLAD.BA",
+        "NKED.BA",
+        "AMZND.BA",
+        "MSFTD.BA",
+        "PBRD.BA",
+    ]
+
+    @st.cache_data(ttl=timedelta(hours=24))
+    def get_all_trades() -> Dict[str, TradeSet]:
+        return all_trades(tickers, timeframe, cache=False)
+
+    try:
+        selected_trades = get_all_trades()
+        selected_tickers = st.multiselect(
+            "Choose tickers", tickers, tickers
+         )
+        if not selected_tickers:
+            st.error("Please select at least one ticker.")
+        else:
+            st.write("#### Signals")
+
+            with st.expander("Signals", expanded=True):
+                with st.container():
+                    col1, col2 = st.columns(2)
+                    col1.markdown("### Long and Short Trends")
+                    col1.markdown("**Trends** are either a _Trend_ (```1```) or _No Trend_ (```0```) depending on the **Trend** passed into ***Trend Signals**")
+                    col1.markdown("The **Trades** are either _Enter_ (```1```) or _Exit_ (```-1```) or _No Position/Action_ (```0```). These are based on the **Trend** passed into **Trend Signals** whether they are _Long_ or _Short_ Trends.")
+
+                    col2.markdown("### Buy and Hold Returns (*PCTRET_1*) vs. Cum. Active Returns (*ACTRET_1*)")
+                for ticker in selected_tickers:
+                    (trades, trendy, asset, long) = selected_trades[ticker]
+
+                    def create_signals_chart():
+                        trades = trendy.TS_Trades.tail(30)
+
+                        source = trades.rename('signal').to_frame().reset_index()
+                        source['symbol'] = ticker
+
+                        chart = alt.Chart(source).mark_area(opacity=0.5).encode(
+                            x=alt.X("Date:T").timeUnit("yearmonthdate").title("date"),
+                            y="signal",
+                            color=alt.Color("symbol")
+                        ).properties(
+                            height=150,
+                        )
+                        return chart
+                    signals_chart = create_signals_chart()
+
+                    def create_trend_chart():
+                        long_trend = trendy.TS_Trends.tail(30)
+
+                        source = long_trend.rename('signal').to_frame().reset_index()
+                        source['symbol'] = ticker
+
+                        chart = alt.Chart(source).mark_line(point=True).encode(
+                            x=alt.X("Date:T").timeUnit("yearmonthdate").title("date"),
+                            y="signal",
+                            color=alt.Color("symbol:N")
+                        ).properties(
+                            height=150,
+                        )
+                        return chart
+                    trend_chart = create_trend_chart()
+
+                    returns = ((asset.tail(30)[["PCTRET_1", "ACTRET_1"]] + 1).cumprod() - 1).reset_index()
+
+                    def create_actret_chart():
+                        source = returns
+
+                        chart = alt.Chart(source).mark_area(opacity=0.7).encode(
+                            x=alt.X("Date:T").timeUnit("yearmonthdate").title("date"),
+                            y="ACTRET_1"
+                        )
+                        pctret_chart = alt.Chart(source).mark_line(point=True).encode(
+                            x=alt.X("Date:T").timeUnit("yearmonthdate").title("date"),
+                            y="PCTRET_1"
+                        )
+                        return chart + pctret_chart
+                    actret_chart = create_actret_chart()
+
+                    with st.container():
+                        col1, col2 = st.columns(2)
+                        col1.altair_chart(trend_chart + signals_chart, use_container_width=True)
+                        col2.altair_chart(actret_chart, use_container_width=True)
+
+            st.write("#### Tickers")
+            with st.expander("Tickers", expanded=False):
+                for ticker in selected_tickers:
+                    (trades, trendy, asset, long) = selected_trades[ticker]
+                    
+                    # chart = asset[["close", "EMA_10", "EMA_20", "EMA_50"]]
+                    # chart.plot(subplots=False, figsize=(16, 10), color=colors("BkGrOrRd"), title=ptitle, grid=True)
+
+                    source = asset.tail(180).reset_index()
+                    close_chart = (
+                        alt.Chart(source)
+                        .mark_line()
+                        .encode(
+                            x="Date:T",
+                            y=alt.Y("close:N")
+                        )
+                    )
+                    ema10_chart = (
+                        alt.Chart(source)
+                        .mark_line()
+                        .encode(
+                            x="Date:T",
+                            y=alt.Y("EMA_10:N")
+                        )
+                    )
+                    st.markdown(f"#### Ticker: {ticker}")
+                    with st.container():
+                        col1, col2 = st.columns(2)
+                        col1.write(asset)
+                        col2.altair_chart(close_chart + ema10_chart, use_container_width=True)
+    except URLError as e:
+        st.error(
+            """
+            **This demo requires internet access.**
+
+            Connection error: %s
+        """
+            % e.reason
+        )
+
+def data_frame_demo():
+    import streamlit as st
+    import pandas as pd
+    import altair as alt
+
+    from urllib.error import URLError
+
+    st.markdown(f"# {list(page_names_to_funcs.keys())[1]}")
+    st.write(
+        """
+        This demo shows how to use `st.write` to visualize Pandas DataFrames.
+
+(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)
+"""
+    )
+
+    @st.cache_data
+    def get_UN_data():
+        AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
+        df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
+        return df.set_index("Region")
+
+    try:
+        df = get_UN_data()
+        countries = st.multiselect(
+            "Choose countries", list(df.index), ["China", "United States of America"]
+        )
+        if not countries:
+            st.error("Please select at least one country.")
+        else:
+            data = df.loc[countries]
+            data /= 1000000.0
+            st.write("### Gross Agricultural Production ($B)", data.sort_index())
+
+            data = data.T.reset_index()
+            data = pd.melt(data, id_vars=["index"]).rename(
+                columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
+            )
+            chart = (
+                alt.Chart(data)
+                .mark_area(opacity=0.3)
+                .encode(
+                    x="year:T",
+                    y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
+                    color="Region:N",
+                )
+            )
+            st.altair_chart(chart, use_container_width=True)
+    except URLError as e:
+        st.error(
+            """
+            **This demo requires internet access.**
+
+            Connection error: %s
+        """
+            % e.reason
+        )
+
+
+page_names_to_funcs = {
+    "Summary": summary,
+    "Dataframe demo": data_frame_demo,
+    "—": intro,
+    "Plotting Demo": plotting_demo
+}
+
+demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
---
+++ notebooks/alerts.py
@@ -0,0 +1,121 @@
+import datetime as dt
+from typing import List, Dict, Tuple
+
+import numpy as np
+import pandas as pd
+# pd.set_option("max_rows", 100)
+# pd.set_option("max_columns", 20)
+
+# import mplfinance as mpf
+import pandas_ta as ta
+
+from . import watchlist
+
+
+TradeSet = Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]
+
+def all_trades(tickers: List[str], timeframe: str, cache=True) -> Dict[str, TradeSet]:
+    watch = watchlist.Watchlist(tickers, tf=timeframe, ds_name="yahoo", timed=True)
+    # watch.strategy = ta.CommonStrategy # If you have a Custom Strategy, you can use it here.
+    watch.load(analyze=True, verbose=False, force=True, cache=cache)
+
+    def recent_bars(df, tf: str = "1y"):
+        # All Data: 0, Last Four Years: 0.25, Last Two Years: 0.5, This Year: 1, Last Half Year: 2, Last Quarter: 4
+        yearly_divisor = {
+            "all": 0,
+            "10y": 0.1,
+            "5y": 0.2,
+            "4y": 0.25,
+            "3y": 1./3,
+            "2y": 0.5,
+            "1y": 1,
+            "6mo": 2,
+            "3mo": 4,
+        }
+        yd = yearly_divisor[tf] if tf in yearly_divisor.keys() else 0
+        return int(ta.RATE["TRADING_DAYS_PER_YEAR"] / yd) if yd > 0 else df.shape[0]
+
+    def trim(ticker):
+        duration = "1y"
+        asset = watch.data[ticker]
+        recent = recent_bars(asset, duration)
+        asset.columns = asset.columns.str.lower()
+        asset.drop(columns=["dividends", "split"], errors="ignore", inplace=True)
+        asset = asset.copy().tail(recent)
+        return asset
+
+    def create_trend(asset):
+        # Example Long Trends
+        # long = ta.sma(asset.close, 50) > ta.sma(asset.close, 200) # SMA(50) > SMA(200) "Golden/Death Cross"
+        # long = ta.sma(asset.close, 10) > ta.sma(asset.close, 20) # SMA(10) > SMA(20)
+        long = ta.ema(asset.close, 10) > ta.ema(asset.close, 20)  # EMA(8) > EMA(21)
+        # long = ta.increasing(ta.ema(asset.close, 50))
+        # long = ta.macd(asset.close).iloc[:,1] > 0 # MACD Histogram is positive
+        # long = ta.amat(asset.close, 50, 200).AMATe_LR_2  # Long Run of AMAT(50, 200) with lookback of 2 bars
+
+        # long &= ta.increasing(ta.ema(asset.close, 50), 2) 
+        # Uncomment for further long restrictions, in this case when EMA(50) is increasing/sloping upwards
+
+        # long = 1 - long # uncomment to create a short signal of the trend
+
+        asset.ta.ema(length=10, sma=False, append=True)
+        asset.ta.ema(length=20, sma=False, append=True)
+        asset.ta.ema(length=50, sma=False, append=True)
+        asset.ta.dema(length=50, sma=False, append=True)
+        # asset.ta.xsignals(asset.ta.ema(), 10, 20, above=True)
+        asset.ta.percent_return(append=True)
+        print("TA Columns Added:")
+        return asset, long
+
+    def create_trade_table(asset, trendy):
+        entries = trendy.TS_Entries * asset.close
+        entries = entries[~np.isclose(entries, 0)]
+        entries.dropna(inplace=True)
+        entries.name = "Entry"
+
+        exits = trendy.TS_Exits * asset.close
+        exits = exits[~np.isclose(exits, 0)]
+        exits.dropna(inplace=True)
+        exits.name = "Exit"
+
+        total_trades = trendy.TS_Trades.abs().sum()
+        rt_trades = int(trendy.TS_Trades.abs().sum() // 2)
+
+        all_trades = trendy.TS_Trades.copy().fillna(0)
+        all_trades = all_trades[all_trades != 0]
+
+        trades = pd.DataFrame({
+            "Signal": all_trades,
+            entries.name: entries,
+            exits.name: exits
+        })
+
+        # Show some stats if there is an active trade (when there is an odd number of round trip trades)
+        if total_trades % 2 != 0:
+            unrealized_pnl = asset.close.iloc[-1] - entries.iloc[-1]
+            unrealized_pnl_pct_change = 100 * ((asset.close.iloc[-1] / entries.iloc[-1]) - 1)
+            print("Current Trade:")
+            print(f"Price Entry | Last:\t{entries.iloc[-1]:.4f} | {asset.close.iloc[-1]:.4f}")
+            print(f"Unrealized PnL | %:\t{unrealized_pnl:.4f} | {unrealized_pnl_pct_change:.4f}%")
+        print(f"\nTrades Total | Round Trip:\t{total_trades} | {rt_trades}")
+        print(f"Trade Coverage: {100 * asset.TS_Trends.sum() / asset.shape[0]:.2f}%")
+
+        return trades
+
+    def create_trend_signals(asset, long):
+        trendy = asset.ta.tsignals(long, asbool=False, append=True)
+        asset["ACTRET_1"] = trendy.TS_Trends.shift(1) * asset.PCTRET_1
+
+        return trendy
+
+    def create_trades(ticker) -> TradeSet:
+        print(f"{ticker} {watch.data[ticker].shape}\nColumns: [', '.join(list(watch.data[ticker].columns))]")
+        asset = trim(ticker)
+        asset, long = create_trend(asset)
+        trendy = create_trend_signals(asset, long)
+        trades = create_trade_table(asset, trendy)
+        print(trades.tail(2))
+        return (trades, trendy, asset, long)
+
+    return {ticker: create_trades(ticker) for ticker in tickers}
+
